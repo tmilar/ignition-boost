0f225688234b1bc91ff33241c82055593caa37dc
tmilar <milar@strands.com>
2016-01-08 19:42:32 -0300

    IB0 Setup script file, to define loading order of scripts - Initial Commit

---
+++ setup.rb
@@ -0,0 +1,21 @@
+#==============================================================================
+# ** TDD Plugins Module - Blank Setup File
+#------------------------------------------------------------------------------
+# Description
+# ===========
+# This is a minified setup file without all the comments and descriptions. If
+# you want to use it in your project instead of the inflated default setup.rb
+# file, just delete setup.rb and rename this file into setup.rb
+#==============================================================================
+plugins = {
+  :src => [  "IB_BOSS_module", "IB_BOSS_game" ]
+}
+
+ROOT_PATH = "ignition-boost"
+load_script "Data/#{ROOT_PATH}/plugins_module.rb" # Do not edit
+
+
+Plugins.root_path = ROOT_PATH
+Plugins.load_recursive(plugins)
+Plugins.package
+load_script("Data/#{ROOT_PATH}/target.rb")
