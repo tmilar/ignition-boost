348c3f6caa349ffc63ff6af0e09cb3a20a6ecde4
tmilar <milar@strands.com>
2016-01-23 05:17:50 -0300

    initial commit - Subject module for Observer pattern

---
+++ lib/misc/subject.rb
@@ -0,0 +1,27 @@
+# require 'forwardable'
+
+module Subject
+  # extend Forwardable
+
+  # def_instance_delegator :@observers, :<<, :add_observer
+  # def_instance_delegator :@observers, :delete, :delete_observer
+
+  def initialize(config = {})
+    @observers = []
+    super(config)
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
+  end
+
+  def notify_observers(msg, data={})
+    @observers.each do |observer|
+      observer.notified(msg, data)
+    end
+  end
+end
