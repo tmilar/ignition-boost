bc99af40ab1fb32df5a65eb23f06bfd53ab75e3c
tmilar <milar@strands.com>
2016-01-24 02:47:56 -0300

    initial commit - utility

---
+++ lib/misc/nil_zero.rb
@@ -0,0 +1,20 @@
+class Object
+  def nil_zero?
+    self.nil? || self == 0
+  end
+
+  def nil_empty?
+    self.nil? || self.empty?
+  end
+end
+
+# # which lets you do
+# nil.nil_zero? # returns true
+# 0.nil_zero?   # returns true
+# 1.nil_zero?   # returns false
+# "a".nil_zero? # returns false
+#
+# unless discount.nil_zero?
+#   # do stuff...
+# end
+# source: http://stackoverflow.com/questions/252203/checking-if-a-variable-is-not-nil-and-not-zero-in-ruby
