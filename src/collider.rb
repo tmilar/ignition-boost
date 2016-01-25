ca3d8bf5efd553161fcae3f68655037312264378
tmilar <milar@strands.com>
2016-01-25 03:12:11 -0300

    IB7-4 Add collider collision checks for ships and lazors

--- src/collider.rb
+++ src/collider.rb
@@ -1,57 +1,42 @@
 
 class Collider
-  attr_accessor :enemies
-  attr_accessor :pups
-  attr_accessor :player
-  attr_accessor :lazors
-  attr_accessor :elazors
 
-  def initialize
-    @enemies = []
-    @pups = []
-    @player = {}
-    @plazors = []
-    @elazors = []
-  end
   # check colissions:
 
   # enemies ->  player, plazors
   # elazors -> player, pups (optional)
   # pups -> player, elazors, (opt) plazors (opt)
 
-  def update
-    check_enemies
-    check_elazors
+
+  # Check elazor with player
+  def self.check_elazor(elazor, player)
+    if collides?(elazor, player)
+      player.elazor_hit(elazor)
+      elazor.player_hit
+    end
   end
 
-  def check_elazors
-    @elazors.each { |elazor|
-      # -> @player
-      if collides?(elazor, @player)
-        elazor.player_hit(@player)
-        @player.elazor_hit(elazor)
-        # TODO delete on collision...
+  def self.check_enemy(enemy, plazors, player)
+    if collides?(enemy, player)
+      Logger.trace("Collided #{enemy}, #{enemy.stats} WITH #{player}, #{player.stats}")
+      enemy.player_collision(player)
+      player.enemy_collision(enemy)
+    end
+
+    plazors.each {
+      |plazor|
+      if collides?(plazor, enemy)
+        Logger.trace("Collided #{plazor}, #{plazor.stats} WITH #{enemy}, #{enemy.stats}")
+        enemy.plazor_hit(plazor)
+        plazor.enemy_hit
       end
     }
   end
 
-  def check_enemies
-    # check enemies
-    @enemies.each { |enemy|
-      # ->  player
-      if collides?(enemy, @player)
-        enemy.player_collision(@player)
-        @player.enemy_collision(enemy)
-      end
 
-      # -> plazors
-      @plazors.each { |plazor|
-        if collides?(enemy, plazor)
-          enemy.plazor_hit(plazor)
-          plazor.enemy_hit
-        end
-      }
-    }
+  def self.collides?(obj1, obj2)
+    # Logger.trace("Rect1: #{obj1.rectangle}, Rect2: #{obj2.rectangle}, collide? #{obj1.rectangle.collide_rect?(obj2.rectangle)}")
+    return obj1.rectangle.collide_rect?(obj2.rectangle)
   end
 
 end
