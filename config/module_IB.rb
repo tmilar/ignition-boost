fe78d10d45f4f0cb2a91f1cbf5fe849c8fd21234
tmilar <milar@strands.com>
2016-02-07 21:40:20 -0300

    Log level '1' (ERROR) for release v1.5.4

--- config/module_IB.rb
+++ config/module_IB.rb
@@ -389,7 +389,7 @@ module IB
 
   DEBUG = {
       # borders: true  ## draw rectangle borders
-      logger_level: 5  # 0 NONE, 1 ERROR, 2 WARN, 3 INFO (recommended), 4 DEBUG, 5 TRACE
+      logger_level: 1  # 0 NONE, 1 ERROR, 2 WARN, 3 INFO (recommended), 4 DEBUG, 5 TRACE
   }
   #-------------------------------------------------------------------------------
   #  BUILD GAME
