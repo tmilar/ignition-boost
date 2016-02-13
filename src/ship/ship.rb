5282f8dbcec7555d147a7fd94a87971cedbf7718
tmilar <milar@strands.com>
2016-02-13 04:23:56 -0300

    IB7-7 Ship effect of powerup 'application' been refactored to module Powerupeable

--- src/ship/ship.rb
+++ src/ship/ship.rb
@@ -4,6 +4,7 @@
 class Ship
   include Subject
   extend Forwardable
+  include Powerupeable
 
   def_delegators :@sprite, :x, :y, :ox, :oy, :zoom_x, :zoom_y, :height, :width, :bitmap
   def_delegators :@sprite, :position, :position=, :rectangle, :rectangle=
@@ -147,12 +148,6 @@ class Ship
     self.dispose
   end
 
-  def apply_pup(pup_effect)
-    Logger.debug("About to apply effect #{pup_effect} on #{self}...")
-    pup_effect.each { |stat, effect|
-      self.property_set(stat, effect)
-    }
-  end
 
   #------------------------------------------------------------------------------#
   #  SHIP PROPERTIES  || OVERRIDE GETTERS & SETTERS
@@ -189,34 +184,6 @@ class Ship
     end
   end
 
-  def property_set(prop, value)
-    getter = prop
-    setter = "#{prop}="
-
-    unless self.respond_to?(setter) && self.respond_to?(getter)
-      Logger.error("Trying to set property #{prop}, #{value} on #{self}, but doesn't respond to #{setter} or #{getter}!")
-      return
-    end
-
-    valid_types = [Integer, Float, Hash]
-    if valid_types.none? { |type| type === value}
-      Logger.error("Unexpected type of value #{value} inputted for #{prop}, needs to be one of: #{valid_types}!")
-      return
-    end
-
-    current = self.send(getter)
-
-    # calculate new value
-    if value.is_a?(Integer)
-      value += current
-    elsif value.is_a?(Float)
-      value *= current
-    end
-
-    # call setter for new value
-    Logger.trace("Calling ':#{setter}' #{value} on #{self} #{"(previous value was: #{current})" if current}")
-    self.send(setter, value)
-  end
   def difficulty
     @elapsed_time
   end
